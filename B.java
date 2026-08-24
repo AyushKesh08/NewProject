cfa2dd40be42e339e0b5f1e66b83aaaa8d41497b
Ayush Kesharwani <ayushkesharwani1708@gmail.com>
2026-08-24 19:00:03 +0530

    more new changes in File B

--- B.java
+++ B.java
@@ -1,5 +1,9 @@
 class B{
     public static void main(String[] args){
         System.out.println("Hello World!!!!.");
+
+        int x = 10001;
+
+        System.out.println(x + "#####");
     }
 }
